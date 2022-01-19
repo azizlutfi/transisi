1069e14d4110508550b82c99c193169f33f4fa75
azizlutfi <aziz8213@students.amikom.ac.id>
2022-01-19 15:01:16 +0700

    request validation rules

---
+++ Laravel-Dasar/app/Http/Requests/CompanyValidation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompanyValidation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required',
+            'logo' => 'required|image|mimes:png|dimensions:min_width=100,min_height=100|max:2048',
+            'website' => 'required'
+        ];
+    }
+}
