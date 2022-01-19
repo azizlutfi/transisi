ebb5a01c106536b92dce57f97046f945cab4def8
azizlutfi <aziz8213@students.amikom.ac.id>
2022-01-19 22:39:30 +0700

    penambahan authorize menjadi true

--- Laravel-Dasar/app/Http/Requests/CompanyValidation.php
+++ Laravel-Dasar/app/Http/Requests/CompanyValidation.php
@@ -13,7 +13,7 @@ class CompanyValidation extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- Laravel-Dasar/app/Http/Requests/EmployeeValidation.php
+++ Laravel-Dasar/app/Http/Requests/EmployeeValidation.php
@@ -13,7 +13,7 @@ class EmployeeValidation extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
